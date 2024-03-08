378db9a6934f714fddd03a84e0582de9d7bd7561
ripplingsnake <144014277+ripplingsnake@users.noreply.github.com>
2024-03-08 01:14:16 +0100

    Update install_first.sh

--- install_first.sh
+++ install_first.sh
@@ -22,68 +22,13 @@ sed -i '/"SourceModInstallPath"/i \
 # Copy return to game mode to Desktop
 cp /etc/skel/Desktop/steamos-gamemode.desktop ~/Desktop/
 
-# Download the Neroreflex files
-wget https://github.com/rog-ally-gaming/linux-chimeraos/releases/download/v6.1.78.nrflx2-1/linux-neroreflex-6.1.78.nrflx2-1-x86_64.pkg.tar.zst
-wget https://github.com/rog-ally-gaming/linux-chimeraos/releases/download/v6.1.78.nrflx2-1/linux-neroreflex-headers-6.1.78.nrflx2-1-x86_64.pkg.tar.zst
 
-# Install the Neroreflex packages
-sudo pacman -U linux-neroreflex-6.1.78.nrflx2-1-x86_64.pkg.tar.zst
-sudo pacman -U linux-neroreflex-headers-6.1.78.nrflx2-1-x86_64.pkg.tar.zst
-
-# Download and install Rogue Enemy
-wget https://github.com/rog-ally-gaming/rogue-enemy/releases/download/v2.2.3-2/rogue-enemy-2.2.3-2-x86_64.pkg.tar.zst
-sudo pacman -U rogue-enemy-2.2.3-2-x86_64.pkg.tar.zst
-
-#fix rogue touchbar
-sudo sed -i '11c\
-touchbar = false;' /etc/ROGueENEMY/config.cfg
-
-# fix rogue bluetooth
-sudo sed -i '12c\
-controller_bluetooth = false;' /etc/ROGueENEMY/config.cfg
-
-
-# Install special Mesa build
-
-# Array of package URLs
-urls=(
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/lib32-libva-mesa-driver-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/lib32-mesa-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/lib32-mesa-debug-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/lib32-mesa-vdpau-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/lib32-opencl-clover-mesa-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/lib32-opencl-rusticl-mesa-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/lib32-vulkan-mesa-layers-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/lib32-vulkan-radeon-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/lib32-vulkan-swrast-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/libva-mesa-driver-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/mesa-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/mesa-debug-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/mesa-vdpau-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/opencl-clover-mesa-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/opencl-rusticl-mesa-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/vulkan-mesa-layers-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/vulkan-radeon-24.0.1-2-x86_64.pkg.tar.zst"
-    "https://github.com/rog-ally-gaming/mesa-chimeraos/releases/download/v24.0.1-2/vulkan-swrast-24.0.1-2-x86_64.pkg.tar.zst"
-)
-
-# Download and install packages
-for url in "${urls[@]}"
-do
-    wget "$url" && sudo pacman -U  "$(basename $url)"
-done
-# Add AMD pstate commands to GRUB
-# Check if the script is run with root privileges
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root. Please use sudo."
-    exit 1
-fi
 
 # Edit the GRUB configuration file
 GRUB_CONFIG="/etc/default/grub"
 
 # Add the parameters to the GRUB command line
-GRUB_CMDLINE="amd_pstate=active amd_prefcore=enable iomem=relaxed"
+GRUB_CMDLINE="amd_pstate=guided amd_prefcore=enable iomem=relaxed"
 
 # Append the parameters to the existing GRUB_CMDLINE_LINUX_DEFAULT
 sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"$GRUB_CMDLINE /" "$GRUB_CONFIG"
